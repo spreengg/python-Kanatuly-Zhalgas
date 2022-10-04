14b37bf3cf338bff53c84029c4de3da48720df9c
spreengg <96519565+spreengg@users.noreply.github.com>
2022-10-04 11:44:31 +0600

    Add files via upload

---
+++ week 5/task 2.py
@@ -0,0 +1,15 @@
+class Name:
+    
+    def init(self,first_name,last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = full_name
+        
+    def getFirst(self):
+        return self.first_name
+        
+    def getLast(self):
+        return self.last_name
+
+      def getFull(self):
+        return first_name+last_name
